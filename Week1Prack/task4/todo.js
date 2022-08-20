e3eee796d92eb3f448b83ba207d4b580f2242297
Tulle Scoffern <S3948272@student.rmit.edu.au>
2022-08-20 16:34:29 +1000

    first commit

---
+++ Week1Prack/task4/todo.js
@@ -0,0 +1,10 @@
+let vue = new Vue({
+    el: "#app",
+    data: {
+        todo: [
+            { title: 'assignment1', checked: true },
+            { title: 'assignment2', checked: false },
+            { title: 'assignment3', checked: false },
+        ],
+    }
+})
